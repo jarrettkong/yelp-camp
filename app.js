7dd090f0d0ff0a21c96a304012e2b9aa1b81ea15
jarrettkong <jarrettkong@me.com>
2018-05-28 16:46:09 -0600

    added basic MongoDB functionality

--- app.js
+++ app.js
@@ -1,41 +1,75 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express = require("express"),
+      app = express(),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose") 
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-let campgrounds = 
-[
-{name: "Golden Trout", img: "http://www.owensvalleyhistory.com/golden_trout/golden_trout_camp01.jpg"},
-{name: "Golden Trout", img: "http://www.owensvalleyhistory.com/golden_trout/golden_trout_camp01.jpg"},
-{name: "Golden Trout", img: "http://www.owensvalleyhistory.com/golden_trout/golden_trout_camp01.jpg"},
-{name: "Golden Trout", img: "http://www.owensvalleyhistory.com/golden_trout/golden_trout_camp01.jpg"},
-{name: "Golden Trout", img: "http://www.owensvalleyhistory.com/golden_trout/golden_trout_camp01.jpg"},
-{name: "Golden Trout", img: "http://www.owensvalleyhistory.com/golden_trout/golden_trout_camp01.jpg"},
-{name: "Golden Trout", img: "http://www.owensvalleyhistory.com/golden_trout/golden_trout_camp01.jpg"}
-];
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  img: String,
+  description: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res) {
-  res.render("index");
+  res.render("home");
 });
 
+// INDEX
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", {campgrounds: campgrounds});
+  // get campgrounds from MongoDB
+    Campground.find({}, function(err, campgrounds) {
+    if(err) {
+      console.log("error found");
+      console.log(err);
+    } else {
+      res.render("index", {campgrounds: campgrounds}); // campgrounds variable from callback function
+    }
+  });
 });
 
+// CREATE
 app.post("/campgrounds", function(req, res) {
-  const newCamp = {name: req.body.name, img: req.body.img};
-  campgrounds.push(newCamp);
-  res.redirect("campgrounds");
-})
+  const newCampground = {
+    name: req.body.name, 
+    img: req.body.img,
+    description: req.body.description
+  };
+  // create new campground and add to DB
+  Campground.create(newCampground, function(err, campground) {
+    if(err) {
+      console.log("error found");
+      console.log(err);
+    } else {
+      console.log("added campground");
+    }
+  });  
+  res.redirect("/campgrounds"); // reload /campgrounds page with GET request
+});
 
+// NEW
 app.get("/campgrounds/new", function(req, res) {
   res.render("new");
 });
 
+// SHOW
+app.get("/campgrounds/:id", function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("show", {campground: campground}); // pass in found campground
+    }
+  });
+});
+
 app.get("/*", function(req, res) {
-  res.send("404 not found");
+  res.send("page not found");
 });
 
 app.listen(3000);
